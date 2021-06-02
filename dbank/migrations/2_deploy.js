5b6e5acec54d5b571041d1387f5a02e87803f572
vasilis <vasilis.green@gmail.com>
2021-06-02 13:43:10 +0200

    dBank WIP

--- dbank/migrations/2_deploy.js
+++ dbank/migrations/2_deploy.js
@@ -1,14 +1,16 @@
+// Import cotracts from the ABI folder
 const Token = artifacts.require("Token");
 const dBank = artifacts.require("dBank");
 
-module.exports = async function(deployer) {
-	//deploy Token
-
-	//assign token into variable to get it's address
-	
-	//pass token address for dBank contract(for future minting)
-
-	//assign dBank contract into variable to get it's address
-
-	//change token's owner/minter from deployer to dBank
+module.exports = async function (deployer) {
+  //deploy Token with truffle
+  await deployer.deploy(Token);
+  // get the token from the blockhain -- assign token into variable to get it's address
+  const token = await Token.deployed();
+  // deploy dbank contract -- pass token address for dBank contract constructor (so the banker can become the minter)
+  await deployer.deploy(dBank, token.address);
+  //assign dBank contract into variable to get it's address
+  const dbank = await dBank.deployed();
+  //change token's owner/minter from deployer to dBank
+  await token.passMinterRole(dbank.address);
 };
--- dbank/src/contracts/Token.sol
+++ dbank/src/contracts/Token.sol
@@ -1,22 +1,39 @@
 // SPDX-License-Identifier: MIT
-// pragma solidity >=0.6.0 <0.8.0;
-pragma solidity ^0.8.1;
-
+pragma solidity >=0.6.0 <0.8.0;
+// pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-  //add minter variable
+    //add minter variable
+    address public minter;
 
-  //add minter changed event
+    //add minter changed event
+    event MinterChanged(address indexed _from, address _to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
-    //asign initial minter
-  }
+    /*
+        ERC20("Name", "Symbol") is the contructor function from the contract e inherit from.
+    */
+    constructor() payable ERC20("Decentralized Bank Coin", "DBC") {
+        //asign initial minter
+        minter = msg.sender;
+    }
 
-  //Add pass minter role function
 
-  function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
-		_mint(account, amount);
+    //Add pass minter role function
+    function passMinterRole(address _to) public returns(bool) {
+        require(minter == msg.sender, "Only the owner can pass minter role");
+        minter == _to;
+        emit MinterChanged(msg.sender, _to);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) public {
+        //check if msg.sender have minter role
+        require(minter == msg.sender, "You are not allowed");
+        /*
+            we inherrit _mint() from ERC20
+            it creates tokens -- in our case 1:1 to ether
+        */
+        _mint(account, amount);
 	}
 }
