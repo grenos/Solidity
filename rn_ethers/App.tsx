7ffea6cb214c9fd4978dcbea418a09b5c9c5db10
vasilis <vasilis.green@gmail.com>
2021-07-07 22:31:39 +0200

    add keychain lib

--- rn_ethers/App.tsx
+++ rn_ethers/App.tsx
@@ -8,11 +8,13 @@ import '@ethersproject/shims';
 // Import the ethers library
 import {ethers} from 'ethers';
 
+import * as Keychain from 'react-native-keychain';
+
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const [provider, setProvider] = useState<ethers.providers.InfuraProvider>();
-  const [wallet, setWallet] = useState<ethers.Wallet>();
+  const [address, setAddress] = useState<String>();
 
   useEffect(() => {
     const providerInstance = new ethers.providers.InfuraProvider('ropsten', {
@@ -23,16 +25,46 @@ const App = () => {
 
     // console.log(walletInstance.privateKey);
     // console.log(walletInstance.address);
-    console.log(walletInstance.mnemonic);
+    // console.log(walletInstance.mnemonic);
 
-    setWallet(walletInstance);
+    setPrivateKeyToKeychain(
+      walletInstance.address,
+      walletInstance.privateKey.substring(2),
+    );
+
+    setAddress(walletInstance.address);
     setProvider(providerInstance);
   }, []);
+
+  const setPrivateKeyToKeychain = async (
+    username: string,
+    password: string,
+  ) => {
+    const result = await Keychain.setGenericPassword(username, password);
+    console.log(result);
+  };
+
+  const getPrivateKeyFromKeychain = async () => {
+    try {
+      const credentials = await Keychain.getGenericPassword();
+      if (credentials) {
+        console.log(
+          'Credentials successfully loaded for user ' + credentials.username,
+        );
+      } else {
+        console.log('No credentials stored');
+      }
+    } catch (error) {
+      console.log("Keychain couldn't be accessed!", error);
+    }
+  };
 
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>TEST PAGE</Text>
+      <Text style={{padding: 100}} onPress={getPrivateKeyFromKeychain}>
+        Press ME please!
+      </Text>
     </SafeAreaView>
   );
 };
